c18484488f77ea9acd759e019e8f00c960efc8a8
Rafael Saldanha <119461894+SaldanhaX@users.noreply.github.com>
2023-01-15 08:59:08 -0300

    Add files via upload

---
+++ Aula 3 - Array e objetos/script.js
@@ -0,0 +1,9 @@
+// o que são vetores ou arrays
+
+// como declará-los
+
+console.log (array)
+
+//pode guardar todo tipo de dado
+let array = ['string', 1, true, ['array1'], ['array22'], ['array72']];
+console.log (array);
