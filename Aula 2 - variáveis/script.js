bb16699183c04652454ec1fe5217f80f29cd1a8e
Rafael Saldanha <119461894+SaldanhaX@users.noreply.github.com>
2023-01-15 08:53:23 -0300

    Add files via upload

---
+++ Aula 2 - variáveis/script.js
@@ -0,0 +1,41 @@
+// primitivos
+
+// boolean
+var VouF = false;
+console.log(typeof(VouF));
+
+// number
+var numeroQualquer = 1;
+console.log(typeof (numeroQualquer));
+
+// string
+var nome = 'rafael'
+console.log(typeof(nome))
+
+// escopo global e local
+
+var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopolocal(){
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+escopolocal
+
+// adição
+var adição = 1 + 1;
+console.log(adição);
+
+// subtração
+var subtração = 82 - 24;
+console.log(subtração);
+
+// divisão real
+
+var divisãoreal = 8 / 4 
+console.log(divisãoreal)
+
+//potenciação
+var potenciação = 2 ** 10
+console.log(potenciação);
